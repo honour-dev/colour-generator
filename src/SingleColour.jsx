8bc2abb9e4ba8fae7dcaee7ff35ba9d345bde74a
Obayuwana Honour Okeoghene <honouroke@gmail.com>
2025-06-08 21:52:18 -0100

    added a copy to clipboard feature by clicking on the hex code

--- src/SingleColour.jsx
+++ src/SingleColour.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {toast} from 'react-toastify'
 
 const SingleColour = ({index, color}) => {
   const {hex, weight} = color;
+  const saveToClipboard = async () => {
+    if(navigator.clipboard){
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success('Colour copied to clipboard')
+      } catch (error) {
+        toast.error('Failed to copy colour to clipboard')
+      }
+    } else{
+      toast.error(`Can't copy this colour`)
+    }
+  }
+
   return (
-    <article className={index > 10 ? 'color color-light' : 'color'} style={{background: `#${hex}`}}>
+    <article className={index > 10 ? 'color color-light' : 'color'} style={{background: `#${hex}`}} onClick={saveToClipboard}>
       <p className="percent-value">{weight}%</p>
       <p className="color-value">#{hex}</p>
     </article>
